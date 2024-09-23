9cdccf43bd281d7912926997f10c5cd231890cc9
Juan Yang <chaijuanyang@gmail.com>
2024-09-23 23:37:08 +0800

    updated index containers

--- vega_lite_vjs.js
+++ vega_lite_vjs.js
@@ -3,6 +3,7 @@ vegaEmbed("#map_plot", vg1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-vegaEmbed("#map_plot_death", vg1).then(function(result) {
+var vg2 = "ne_10m_admin_1_states_provinces.json";
+vegaEmbed("#map_plot_death", vg2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
