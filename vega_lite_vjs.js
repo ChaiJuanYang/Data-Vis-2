68abdee0400b0cd91b692774deff00cf6f38bb06
Juan Yang <chaijuanyang@gmail.com>
2024-10-02 02:14:54 +0800

    Heatmap Births

--- vega_lite_vjs.js
+++ vega_lite_vjs.js
@@ -7,3 +7,9 @@ var vg2 = "map_plot_death.json";
 vegaEmbed("#map_plot_death", vg2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+
+var vg3 = "heatmap_birth.json";
+vegaEmbed("#heatmap_birth", vg3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
